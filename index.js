6358841a51907b40fa94753479bdef579f7873e4
Yongye Tan <yongyetan1209@gmail.com>
2024-03-27 19:45:15 -0400

    trigger first workflow

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/iceCream', (req, res) => {
+    res.send('Hello I love ice cream');
+});
+
 app.listen(8080, () => {
     console.log('Server is running on http://localhost:8080');
 });
