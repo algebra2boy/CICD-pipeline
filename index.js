26eb12dc252b0f03a080131ba0f6c43e44876e12
Yongye Tan <yongyetan1209@gmail.com>
2024-03-27 19:41:41 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(8080, () => {
+    console.log('Server is running on http://localhost:8080');
+});
+
+export default app;
