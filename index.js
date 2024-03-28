94154be31aefeb7538f2662f57c0fe7c19493439
Yongye Tan <yongyetan1209@gmail.com>
2024-03-27 20:20:58 -0400

    update eslint

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.send('Hello World');
 });
 
 app.get('/iceCream', (req, res) => {
-    res.send('Hello I love ice cream');
+  let x = 0;
+  res.send('Hello I love ice cream');
 });
 
 app.listen(8080, () => {
-    console.log('Server is running on http://localhost:8080');
+  console.log('Server is running on http://localhost:8080');
 });
 
 export default app;
